648f80f127eda4d2c7f8176ee3da61cd085d33f2
baloodubronx <julien.autebert@gmail.com>
2015-04-23 19:06:59 +0200

    working on the editor

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -4,10 +4,5 @@ angular.module('xstaticApp')
 .controller( 'HomeCtrl', HomeCtrl);
 
 function HomeCtrl( $scope, socket ) {
-
-  $scope.description = "A minimal template for angular";
-  socket.on('message', function (message) {
-    $scope.description = message;
-    socket.emit('lapin', 'crétin');
-  });
+  
 }
